3440db9473e5702af4b5e9da63d60b9d6558bc30
PoisonNF <903131263@qq.com>
2023-01-12 16:31:34 +0800

    完善数据插入和更新，优化ReceivePTE显示

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     /*读取可使用的串口号*/
     QStringList serialNamePort;
     serialPort = new QSerialPort(this);
@@ -18,12 +19,9 @@ MainWindow::MainWindow(QWidget *parent)
     connect(serialPort,SIGNAL(readyRead()),this,SLOT(serialPortReadReady_Slot()));
 
     /*SQLite相关初始化*/
-    //创建数据库
-    createDB();
-    //创建数据表
-    createTable();
-    //查询
-    queryTable();
+    createDB();     //创建数据库
+    createTable();  //创建数据表
+    queryTable();   //查询
 }
 
 MainWindow::~MainWindow()
@@ -31,7 +29,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//创建数据库
+/*创建数据库*/
 void MainWindow::createDB()
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
@@ -45,7 +43,7 @@ void MainWindow::createDB()
         qDebug() << "创建或者打开数据库失败！";
     }
 }
-//创建数据表
+/*创建数据表*/
 void MainWindow::createTable()
 {
     QSqlQuery query;
@@ -56,7 +54,7 @@ void MainWindow::createTable()
                           "Y REAL,"
                           "Z REAL)");
     query.exec(jystr);
-    qDebug() << "jy901数据库创建";
+    qDebug() << "jy901数据表创建";
 
     QString rmstr = QString("CREATE TABLE rm3100 ("
                             "ID INT PRIMARY KEY NOT NULL,"
@@ -64,9 +62,9 @@ void MainWindow::createTable()
                             "Y REAL,"
                             "Z REAL)");
     query.exec(rmstr);
-    qDebug() << "rm3100数据库创建";
+    qDebug() << "rm3100数据表创建";
 }
-//查询
+/*查询*/
 void MainWindow::queryTable()
 {
 
@@ -93,13 +91,13 @@ void MainWindow::queryTable()
     ui->RM3100TB->setAlternatingRowColors(true);
 }
 
-//开启槽函数
+/*开启槽函数*/
 void MainWindow::on_OpenPB_clicked()
 {
-    QSerialPort::BaudRate baudRate;
-    QSerialPort::DataBits dataBits;
-    QSerialPort::StopBits stopBits;
-    QSerialPort::Parity checkBits;
+    QSerialPort::BaudRate baudRate = QSerialPort::Baud115200;
+    QSerialPort::DataBits dataBits = QSerialPort::Data8;
+    QSerialPort::StopBits stopBits = QSerialPort::OneStop;
+    QSerialPort::Parity checkBits  = QSerialPort::NoParity;
 
     //获取波特率控件上面的数据
     if(ui->BaudCB->currentText() == "9600") baudRate = QSerialPort::Baud9600;
@@ -138,31 +136,33 @@ void MainWindow::on_OpenPB_clicked()
 
 }
 
-//关闭槽函数
+/*关闭槽函数*/
 void MainWindow::on_ClosePB_clicked()
 {
     serialPort->close();
     QMessageBox::information(this,"提示",ui->UartCB->currentText()+"已关闭");
 }
 
-//发送槽函数
+/*发送槽函数*/
 void MainWindow::on_SendPB_clicked()
 {
     serialPort->write(ui->SendLE->text().toLocal8Bit().data());
 }
 
-//清空槽函数
+/*清空槽函数*/
 void MainWindow::on_ClearPB_clicked()
 {
     ui->ReceivePTE->clear();
 }
 
-//串口准备接收槽函数
+/*串口准备接收槽函数*/
 void MainWindow::serialPortReadReady_Slot()
 {
     QString buf;
-    buf = QString(serialPort->readAll());
-    ui->ReceivePTE->appendPlainText(buf);
+    buf = QString(serialPort->readLine());
+    ui->ReceivePTE->ensureCursorVisible(); //通过滚动文本编辑确保光标可见,始终显示最新一行
+    ui->ReceivePTE->insertPlainText(buf);
+
 
     //以空格进行分割
     QStringList data = buf.split(u' ');
@@ -170,28 +170,47 @@ void MainWindow::serialPortReadReady_Slot()
     //如果是JY901的数据
     if(data.at(0) == 'J')
     {
-        QString updatestr = QString("UPDATE jy901 SET Data = '%2', X = %3, Y = %4, Z = %5 WHERE ID = %1")
-                .arg(data.at(1)).arg(data.at(2)).arg(data.at(3)).arg(data.at(4)).arg(data.at(5));
-        QString str = QString("INSERT INTO jy901 VALUES(%1,'%2',%3,%4,%5)")
-                .arg(data.at(1)).arg(data.at(2)).arg(data.at(3)).arg(data.at(4)).arg(data.at(5));
-        QString deletestr = QString("DELETE FROM jy901 WHERE ID = %1").arg(data.at(1));
-        QSqlQuery query;
-        query.exec(str);
-        query.exec(updatestr);
+        //查询数据表中是否已经存在数据
+        QString checkstr = QString("SELECT * FROM jy901 WHERE ID = '%1'").arg(data.at(1));
+        QSqlQuery query(checkstr);
+        if(query.next()) //存在数据，执行更新操作
+        {
+            QString updatestr = QString("UPDATE jy901 SET Data = '%2', X = %3, Y = %4, Z = %5 WHERE ID = %1")
+                    .arg(data.at(1),data.at(2),data.at(3),data.at(4),data.at(5));
+            query.exec(updatestr);
+        }
+        else
+        {
+            QString insertstr = QString("INSERT INTO jy901 VALUES(%1,'%2',%3,%4,%5)")
+                    .arg(data.at(1),data.at(2),data.at(3),data.at(4),data.at(5));
+            query.exec(insertstr);
+        }
+
+        //QString deletestr = QString("DELETE FROM jy901 WHERE ID = %1").arg(data.at(1));
         //query.exec(deletestr);
     }
 
+    //如果是RM3100的数据
     if(data.at(0) == 'R')
     {
-        QString updatestr = QString("UPDATE rm3100 SET X = %2, Y = %3, Z = %4 WHERE ID = %1")
-                .arg(data.at(1)).arg(data.at(2)).arg(data.at(3)).arg(data.at(4));
-        QString str = QString("INSERT INTO rm3100 VALUES(%1,%2,%3,%4)")
-                .arg(data.at(1)).arg(data.at(2)).arg(data.at(3)).arg(data.at(4));
-        QString deletestr = QString("DELETE FROM rm3100 WHERE ID = %1").arg(data.at(1));
-        QSqlQuery query;
-        query.exec(str);
-        query.exec(updatestr);
+        //查询数据表中是否已经存在数据
+        QString checkstr = QString("SELECT * FROM rm3100 WHERE ID = '%1'").arg(data.at(1));
+        QSqlQuery query(checkstr);
+        if(query.next()) //存在数据，执行更新操作
+        {
+            QString updatestr = QString("UPDATE rm3100 SET X = %2, Y = %3, Z = %4 WHERE ID = %1")
+                    .arg(data.at(1),data.at(2),data.at(3),data.at(4));
+            query.exec(updatestr);
+        }
+        else //不存在数据，执行插入操作
+        {
+            QString insertstr = QString("INSERT INTO rm3100 VALUES(%1,%2,%3,%4)")
+                    .arg(data.at(1),data.at(2),data.at(3),data.at(4));
+            query.exec(insertstr);
+        }
+        //QString deletestr = QString("DELETE FROM rm3100 WHERE ID = %1").arg(data.at(1));
         //query.exec(deletestr);
     }
-    queryTable();
+    queryTable();   //显示到Table View上
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -25,30 +25,30 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    QSerialPort *serialPort; //定义一个SerialPort
+    QSerialPort *serialPort;    //定义一个SerialPort
 private slots:
-    //开启槽函数
-    void on_OpenPB_clicked();
-    //关闭槽函数
-    void on_ClosePB_clicked();
-    //发送槽函数
-    void on_SendPB_clicked();
-    //清空槽函数
-    void on_ClearPB_clicked();
-    //串口准备接收槽函数
-    void serialPortReadReady_Slot();
+
+    void on_OpenPB_clicked();   //开启槽函数
+
+    void on_ClosePB_clicked();  //关闭槽函数
+
+    void on_SendPB_clicked();   //发送槽函数
+
+    void on_ClearPB_clicked();  //清空槽函数
+
+    void serialPortReadReady_Slot();    //串口准备接收槽函数
 private:
     Ui::MainWindow *ui;
-    //创建数据库
-    void createDB();
-    //创建数据表
-    void createTable();
-    //查询
-    void queryTable();
-    QSqlDatabase db;//建立QT和数据库连接
-    QSqlQueryModel JYmodel;//保存JY901结果集
-    QSqlQueryModel RMmodel;//保存RM3100结果集
 
-    QStringList data;//保存被切割数据
+    void createDB();    //创建数据库
+
+    void createTable(); //创建数据表
+
+    void queryTable();  //查询
+
+    QSqlDatabase db;        //建立QT和数据库连接
+    QSqlQueryModel JYmodel; //保存JY901结果集
+    QSqlQueryModel RMmodel; //保存RM3100结果集
+    QStringList data;       //保存被切割数据
 };
 #endif // MAINWINDOW_H
